fba05e6395bbacd6d60ab43156720306690b227d
Oron Trabelsi <orontr@ac.sce.ac.il>
2019-03-24 22:14:19 +0200

    TO DO VMUserRegister
    todo foe VMUserRegister

--- ProjectManagement/ProjectManagement/ViewModel/VMUserRegister.cs
+++ ProjectManagement/ProjectManagement/ViewModel/VMUserRegister.cs
@@ -9,6 +9,7 @@ namespace ProjectManagement.ViewModel
 {
     public class VMUserRegister
     {
+        //TODO: regex for password, at least one capial ,one number and one small letter
         [Required(ErrorMessage = "שדה חובה")]
         [RegularExpression("[a-zA-Z0-9]+$", ErrorMessage = "אותיות באנגלית, לפחות אחת גדולה ולפחות אחת קטנה, לפחות ספרה אחת")]
         [StringLength(20, MinimumLength = 8, ErrorMessage = "לפחות 8 תווים לכל היותר 20")]
