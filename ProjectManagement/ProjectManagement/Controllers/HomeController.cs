6900814423867bb7ea29fc72b8a3e4929ce09773
Oron Trabelsi <orontr@ac.sce.ac.il>
2019-03-24 22:13:28 +0200

    Register page
    מסך ההרשמה , שימו לב שיש VIEW חדש

--- ProjectManagement/ProjectManagement/Controllers/HomeController.cs
+++ ProjectManagement/ProjectManagement/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using ProjectManagement.Models;
+using ProjectManagement.ViewModel;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +10,23 @@ namespace ProjectManagement.Controllers
 {
     public class HomeController : Controller
     {
+        //public ActionResult RedirectByUser()
+        //{
+        //    if (Session["CurrentUser"] != null)
+        //    {
+        //        User currentUsr = (User)(Session["CurrentUser"]);
+        //        if (currentUsr.IsManager)
+        //            return RedirectToAction("ShowManagerPage", "Manager");
+        //        else
+        //            return RedirectToAction("ShowRoommatePage", "Roommate");
+        //    }
+        //    else
+        //    {
+        //        TempData["notAuthorized"] = "אין הרשאה!";
+        //        return RedirectToAction("HomePage");
+        //    }
+        //}
+
         public ActionResult ShowHomePage()
         {
             if (Session["CurrentUser"] != null)
@@ -16,7 +34,14 @@ namespace ProjectManagement.Controllers
             UserLogin usr = new UserLogin();
             return View(usr);
         }
-
+        public ActionResult Register()
+        {
+            if (Session["CurrentUser"] != null)
+                return RedirectToAction("RedirectByUser");
+            VMUserRegister newUsr = new VMUserRegister();
+            newUsr.NewUser = new User();
+            return View(newUsr);
+        }
 
     }
 }
---
+++ ProjectManagement/ProjectManagement/ViewModel/VMUserRegister.cs
@@ -0,0 +1,25 @@
+﻿using ProjectManagement.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ProjectManagement.ViewModel
+{
+    public class VMUserRegister
+    {
+        [Required(ErrorMessage = "שדה חובה")]
+        [RegularExpression("[a-zA-Z0-9]+$", ErrorMessage = "אותיות באנגלית, לפחות אחת גדולה ולפחות אחת קטנה, לפחות ספרה אחת")]
+        [StringLength(20, MinimumLength = 8, ErrorMessage = "לפחות 8 תווים לכל היותר 20")]
+        public string Password { get; set; }
+        //TODO: regex for password, at least one capial ,one number and one small letter
+        [Required(ErrorMessage = "שדה חובה")]
+        [RegularExpression("[a-zA-Z0-9]+$", ErrorMessage = "אותיות באנגלית, לפחות אחת גדולה ולפחות אחת קטנה, לפחות ספרה אחת")]
+        [StringLength(20, MinimumLength = 8, ErrorMessage = "לפחות 8 תווים לכל היותר 20")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "הזן את אותה סיסמה")]
+        public string ConfirmPassword { get; set; }
+        public User NewUser { get; set; }
+    }
+}
