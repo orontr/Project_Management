c5373a134058d3e73659c3b47b07569c49f6f8dd
Oron Trabelsi <orontr@ac.sce.ac.il>
2019-03-24 16:55:08 +0200

    UserLogin and User
    תבדקו שאתם מוסיפים את הקבצים

---
+++ ProjectManagement/ProjectManagement/Models/User.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ProjectManagement.Models
+{
+    public class User:UserLogin
+    {
+        public int Team { get; set; }
+        [RegularExpression("[C|D|M]$", ErrorMessage = "")]
+        [StringLength(1, ErrorMessage = "")]
+        public string Type { get; set; }
+        [Required(ErrorMessage = "Requierd!")]
+        [RegularExpression(@" ^ ([\w\.\-] +)@([\w\-] +)((\.(\w){2, 3})+)$", ErrorMessage = "Please enter a valid email address")]
+        [StringLength(150, ErrorMessage = "Max 150 characters")]
+        public String Email { get; set; }
+        [Required(ErrorMessage = "Requierd!")]
+        [RegularExpression("^[0-9a-zA-Z]+$", ErrorMessage = "Username can contain only numbers and letters!")]
+        [StringLength(20, ErrorMessage = "Max 20 characters")]
+        public string FirstName { get; set; }
+        [Required(ErrorMessage = "Requierd!")]
+        [RegularExpression("^[0-9a-zA-Z]+$", ErrorMessage = "Username can contain only numbers and letters!")]
+        [StringLength(20, ErrorMessage = "Max 20 characters")]
+        public string LastName { get; set; }
+    }
+}
---
+++ ProjectManagement/ProjectManagement/Models/UserLogin.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ProjectManagement.Models
+{
+    public class UserLogin
+    {
+        [Key]
+        [Required(ErrorMessage = "Requierd!")]
+        [RegularExpression("^[0-9a-zA-Z]+$", ErrorMessage = "Username can contain only numbers and letters!")]
+        [StringLength(20, ErrorMessage = "Max 20 characters")]
+        public string UserName { get; set; }
+        //TODO: regex for password, at least one capial ,one number and one small letter
+        [Required(ErrorMessage = "Requierd!")]
+        [RegularExpression("[a-zA-Z0-9]+$", ErrorMessage = "Must to contain at least capital letter, small letter and number")]
+        [StringLength(20, MinimumLength = 8, ErrorMessage = "min 8 max 20 characters")]
+        public string Password { get; set; }
+    }
+}
