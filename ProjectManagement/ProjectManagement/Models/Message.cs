d38ed751103a5986b34afccf70b945bb110b36bd
matanbe4 <34807546+matanbe4@users.noreply.github.com>
2019-03-24 16:55:58 +0200

    Updated Message.cs
    תוסיפו את הקובץ לתיקייה של המודלס

--- ProjectManagement/ProjectManagement/Models/Message.cs
+++ ProjectManagement/ProjectManagement/Models/Message.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,10 +9,19 @@ namespace ProjectManagement.Models
     public class Message
     {
         public DateTime DateOfMessage { get; set; }
+        [Key]
         public int NumberOfMessage { get; set; }
+        [Required]
+        [StringLength(300, ErrorMessage = "Maximum 300 characters.")]
         public String message { get; set; }
+        [Required]
+        [StringLength(35, ErrorMessage = "Maximum 35 characters.")]
         public String Subject { get; set; }
+        [Required]
+        [StringLength(35, ErrorMessage = "Maximum 35 characters.")]
         public string Sender { get; set; }
+        [Required]
+        [StringLength(35, ErrorMessage = "Maximum 35 characters.")]
         public string Receiver { get; set; }
 
     }
