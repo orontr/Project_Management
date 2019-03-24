5e8dc26da7a76ec0d14342111aac038b0466faa2
Oron Trabelsi <orontr@ac.sce.ac.il>
2019-03-24 22:20:41 +0200

    Create UserDal.cs
    הוספת USERDAL

---
+++ ProjectManagement/ProjectManagement/DAL/UserDal.cs
@@ -0,0 +1,19 @@
+﻿using ProjectManagement.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ProjectManagement.DAL
+{
+    public class UserDal : WebContext
+    {
+        public DbSet<User> Users { get; set; }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<User>().ToTable("user");
+        }
+    }
+}
