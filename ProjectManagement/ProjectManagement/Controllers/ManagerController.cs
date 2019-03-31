01b887b6cfcc23514b7829adc6488dc9c0b06761
matanbe4 <34807546+matanbe4@users.noreply.github.com>
2019-03-31 15:14:20 +0300

    T18-71 Manager Profile

--- ProjectManagement/ProjectManagement/Controllers/ManagerController.cs
+++ ProjectManagement/ProjectManagement/Controllers/ManagerController.cs
@@ -1,4 +1,5 @@
-﻿using ProjectManagement.Models;
+﻿using ProjectManagement.DAL;
+using ProjectManagement.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -23,6 +24,64 @@ namespace ProjectManagement.Controllers
             if (!Authorize())
                 return RedirectToAction("RedirectByUser", "Home");
             return View();
+        }
+        public ActionResult ShowManagerProfile()
+        {
+            if (!Authorize())
+                return RedirectToAction("RedirectByUser", "Home");
+            return View((User)Session["CurrentUser"]);
+        }
+        [HttpPost]
+        public ActionResult UpdateManagerProfileSubmit(UserUpdate usr)
+        {
+            if (Session["CurrentUser"] == null)
+                return RedirectToAction("RedirectByUser");
+            User CurrentUser = (User)Session["CurrentUser"];
+            TryValidateModel(usr);
+            if (ModelState.IsValid)
+            {
+
+                UserDal usrDal = new UserDal();
+                User updateUser = usrDal.Users.FirstOrDefault(x => x.UserName == CurrentUser.UserName);
+                updateUser.FirstName = usr.FirstName;
+                updateUser.LastName = usr.LastName;
+                updateUser.Email = usr.Email;
+                usrDal.SaveChanges();
+                TempData["Update"] = "השינויים בוצעו בהצלחה";
+                Session["CurrentUser"] = usrDal.Users.FirstOrDefault(x => x.UserName == CurrentUser.UserName);
+                return View("ShowManagerProfile");
+            }
+            TempData["notUpdate"] = "לא בוצעו שינוים!";
+            return RedirectToAction("ShowManagerProfile");
+
+        }
+
+        public ActionResult UpdatePassword()
+        {
+            if (!Authorize())
+                return RedirectToAction("RedirectByUser", "Home");
+            return View(new UpdatePass());
+        }
+        [HttpPost]
+        public ActionResult UpdateManagerPassSubmit(UpdatePass pass)
+        {
+            if (Session["CurrentUser"] == null)
+                return RedirectToAction("RedirectByUser");
+            User CurrentUser = (User)Session["CurrentUser"];
+            TryValidateModel(pass);
+            if (ModelState.IsValid && pass.OldPassword == CurrentUser.Password)
+            {
+
+                UserDal usrDal = new UserDal();
+                User updateUser = usrDal.Users.FirstOrDefault(x => x.UserName == CurrentUser.UserName);
+                updateUser.Password = pass.NewPassword;
+                usrDal.SaveChanges();
+                TempData["Update"] = "הסיסמה שונתה בהצלחה";
+                return RedirectToAction("ShowManagerProfile");
+            }
+            TempData["notUpdate"] = "לא בוצע שינוי!";
+            return RedirectToAction("ShowManagerProfile");
+
         }
     }
 }     
