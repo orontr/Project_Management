afb712547578bef8a72e3671e4e5c93e430bd175
dolev1894 <dolevda@ac.sce.ac.il>
2019-05-19 19:04:54 +0300

    Update Forum.cs
    עדכון מודל forum

--- ProjectManagement/ProjectManagement/Models/Forum.cs
+++ ProjectManagement/ProjectManagement/Models/Forum.cs
@@ -4,3 +4,16 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
+namespace ProjectManagement.Models
+{
+    public class Forum
+    {
+
+        [Key]
+        public DateTime DateOfPost { get; set; }
+        public int CourseNumber { get; set; }
+        public string Post { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+    }
+}
