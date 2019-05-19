4242618edacb9c192a99536677ac618f027de370
dolev1894 <dolevda@ac.sce.ac.il>
2019-05-19 19:07:03 +0300

    Update Groups.cs
    עדכון מודל group

--- ProjectManagement/ProjectManagement/Models/Groups.cs
+++ ProjectManagement/ProjectManagement/Models/Groups.cs
@@ -3,12 +3,3 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace ProjectManagement.Models
-{
-    public class Groups
-    {
-        public int Team { get; set; }
-        public string Developer { get; set; }
-        public string Client { get; set; }
-    }
-}
