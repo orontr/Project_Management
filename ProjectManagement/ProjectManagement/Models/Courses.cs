a2b50b010ee78d151db502fa041991512bbc23f2
dolev1894 <dolevda@ac.sce.ac.il>
2019-05-19 19:01:44 +0300

    Update Courses.cs
    עדכון מודל courses

--- ProjectManagement/ProjectManagement/Models/Courses.cs
+++ ProjectManagement/ProjectManagement/Models/Courses.cs
@@ -3,15 +3,3 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
-
-namespace ProjectManagement.Models
-{
-    public class Courses
-    {
-        [Key]
-        public int CourseID { get; set; }
-        public string userName  { get; set; }
-        public string name { get; set; }
-        
-    }
-}
