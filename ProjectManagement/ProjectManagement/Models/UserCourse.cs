8e9c20461b0f326bf88091100e2bc84aa38a4a29
dolev1894 <dolevda@ac.sce.ac.il>
2019-05-19 19:32:34 +0300

    Update UserCourse.cs
    T18-10

--- ProjectManagement/ProjectManagement/Models/UserCourse.cs
+++ ProjectManagement/ProjectManagement/Models/UserCourse.cs
@@ -4,3 +4,14 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
+
+namespace ProjectManagement.Models
+{
+    public class UserCourse
+    {
+        [Key, Column(Order = 0)]
+        public int courseNumber { get; set; }
+        [Key, Column(Order = 1)]
+        public string userName { get; set; }
+    }
+}
