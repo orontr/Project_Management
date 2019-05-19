115d23dbd3b7397b06e77fc9db53cbb8d317b71a
dolev1894 <dolevda@ac.sce.ac.il>
2019-05-19 19:11:52 +0300

    Update Message.cs
    עדכון מודל Message

--- ProjectManagement/ProjectManagement/Models/Message.cs
+++ ProjectManagement/ProjectManagement/Models/Message.cs
@@ -3,20 +3,3 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
-
-namespace ProjectManagement.Models
-{
-    public class Message
-    {
-        public DateTime DateAndTime { get; set; }
-        [Key]
-        public int NumOfMessage { get; set; }
-  
-        public String TextMessage { get; set; }
-      
-        public string Sender { get; set; }
-       
-        public string Receiver { get; set; }
-
-    }
-}
