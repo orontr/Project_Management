cad884889e123fd7fba23737e722332314cfc3d5
dolev1894 <dolevda@ac.sce.ac.il>
2019-05-19 19:14:54 +0300

    Update UpdatePass.cs
    עדכון מודל UpdatePass

--- ProjectManagement/ProjectManagement/Models/UpdatePass.cs
+++ ProjectManagement/ProjectManagement/Models/UpdatePass.cs
@@ -4,25 +4,3 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
-namespace ProjectManagement.Models
-{
-    public class UpdatePass
-    {
-        [Required(ErrorMessage = "שדה חובה")]
-        [RegularExpression("[a-zA-Z0-9]+$", ErrorMessage = "אותיות באנגלית, לפחות אחת גדולה ולפחות אחת קטנה, לפחות ספרה אחת")]
-        [StringLength(20, MinimumLength = 8, ErrorMessage = "לפחות 8 תווים לכל היותר 20")]
-        public string OldPassword { get; set; }
-        //TODO: regex for password, at least one capial ,one number and one small letter
-        [Required(ErrorMessage = "שדה חובה")]
-        [RegularExpression("[a-zA-Z0-9]+$", ErrorMessage = "אותיות באנגלית, לפחות אחת גדולה ולפחות אחת קטנה, לפחות ספרה אחת")]
-        [StringLength(20, MinimumLength = 8, ErrorMessage = "לפחות 8 תווים לכל היותר 20")]
-        [DataType(DataType.Password)]
-        public string NewPassword { get; set; }
-        [Required(ErrorMessage = "שדה חובה")]
-        [RegularExpression("[a-zA-Z0-9]+$", ErrorMessage = "אותיות באנגלית, לפחות אחת גדולה ולפחות אחת קטנה, לפחות ספרה אחת")]
-        [StringLength(20, MinimumLength = 8, ErrorMessage = "לפחות 8 תווים לכל היותר 20")]
-        [DataType(DataType.Password)]
-        [Compare("NewPassword", ErrorMessage = "הזן את אותה סיסמה")]
-        public string ConfirmPassword { get; set; }
-    }
-}
