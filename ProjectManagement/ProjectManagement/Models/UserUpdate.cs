e387f3bf1d88983bf3a1e5f3475cc397424c28b8
dolev1894 <dolevda@ac.sce.ac.il>
2019-05-19 19:41:12 +0300

    Update UserUpdate.cs
    T18-12

--- ProjectManagement/ProjectManagement/Models/UserUpdate.cs
+++ ProjectManagement/ProjectManagement/Models/UserUpdate.cs
@@ -4,3 +4,21 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
+namespace ProjectManagement.Models
+{
+    public class UserUpdate
+    {
+        [Required(ErrorMessage = "שדה חובה")]
+        [RegularExpression(@"[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])", ErrorMessage = "הכנס מייל חוקי")]
+        [StringLength(150, ErrorMessage = "מקסימום 150 תווים")]
+        public String Email { get; set; }
+        [Required(ErrorMessage = "שדה חובה")]
+        [RegularExpression("^[a-zA-Z]+$", ErrorMessage = "First name can contain only letters!")]
+        [StringLength(50, ErrorMessage = "מקסימום 50 תווים")]
+        public string FirstName { get; set; }
+        [Required(ErrorMessage = "שדה חובה")]
+        [RegularExpression("^[a-zA-Z]+$", ErrorMessage = "Last name can contain only letters!")]
+        [StringLength(20, ErrorMessage = "מקסימום 20 תווים")]
+        public string LastName { get; set; }
+    }
+}
