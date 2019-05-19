aaf336a34c6dacb86d05381046822379e55397bd
wardasy <34807213+wardasy@users.noreply.github.com>
2019-05-19 22:52:41 +0300

    t18-12

--- ProjectManagement/ProjectManagement/Controllers/ManagerController.cs
+++ ProjectManagement/ProjectManagement/Controllers/ManagerController.cs
@@ -89,9 +89,8 @@ namespace ProjectManagement.Controllers
         {
             User CurrentUser = (User)Session["CurrentUser"];
             VMYourCourses obj = new VMYourCourses();
-            obj.courses = (from c in (new CoursesDal().courses)
-                           where c.userName == CurrentUser.UserName
-                           select c).ToList<Courses>();
+            obj.courses =   c.userName == CurrentUser.UserName
+                           .ToList<Courses>();
             return View(obj);
         }
         public ActionResult AddCourseSub(VMYourCourses c)
