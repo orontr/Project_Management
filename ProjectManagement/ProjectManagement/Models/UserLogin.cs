30ee39e744471be5566a9e8f1686a137c33011fe
dolev1894 <dolevda@ac.sce.ac.il>
2019-05-19 19:36:58 +0300

    Update UserLogin.cs
    T18-129

--- ProjectManagement/ProjectManagement/Models/UserLogin.cs
+++ ProjectManagement/ProjectManagement/Models/UserLogin.cs
@@ -4,3 +4,19 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
+namespace ProjectManagement.Models
+{
+    public class UserLogin
+    {
+        [Key]
+        [Required(ErrorMessage = "שדה חובה")]
+        [RegularExpression("^[0-9a-zA-Z]+$", ErrorMessage = "שם משתמש מכיל אותיות באנגלית ומספרים בלבד")]
+        [StringLength(20, ErrorMessage = "מקסימום 20 תווים")]
+        public string UserName { get; set; }
+        //TODO: regex for password, at least one capial ,one number and one small letter
+        [Required(ErrorMessage = "שדה חובה")]
+        [RegularExpression("[a-zA-Z0-9]+$", ErrorMessage = "סיסמה מכילה אותיות באנגלית ומספרים בלבד")]
+        [StringLength(20, MinimumLength = 8, ErrorMessage = "לפחות 8 תווים , מקסימום 20 תווים")]
+        public string Password { get; set; }
+    }
+}
