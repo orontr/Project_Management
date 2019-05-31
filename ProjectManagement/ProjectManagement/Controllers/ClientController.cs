acba59830cd4835cdc7beaed3d7d84c0bc145619
Oron Trabelsi <orontr@ac.sce.ac.il>
2019-05-31 10:42:33 +0300

    a

--- ProjectManagement/ProjectManagement/Controllers/ClientController.cs
+++ ProjectManagement/ProjectManagement/Controllers/ClientController.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
- 
+
 namespace ProjectManagement.Controllers
 {
     public class ClientController : Controller
@@ -28,56 +28,95 @@ namespace ProjectManagement.Controllers
                 return RedirectToAction("RedirectByUser", "Home");
             return View();
         }
-   
 
-    public ActionResult MessagesPage()
-    {
+
+        public ActionResult ChooseForm()
         {
             if (!Authorize())
                 return RedirectToAction("RedirectByUser", "Home");
-            return View();
+            User usr = (User)Session["CurrentUser"];
+            GroupsDal grpdal = new GroupsDal();
+            List<string> a = (from Groups g in grpdal.groups
+                              where usr.UserName == g.Client
+                              select g.Developer1).ToList<string>();
+            a.AddRange((from Groups g in grpdal.groups
+                        where usr.UserName == g.Client
+                        select g.Developer2).ToList<string>());
+            a.AddRange((from Groups g in grpdal.groups
+                        where usr.UserName == g.Client
+                        select g.Developer2).ToList<string>());
+            HashSet<string> b = new HashSet<string>();
+            foreach (string c in a)
+                b.Add(c);
+            FormDal frmdal = new FormDal();
+            List<Form> formss = (from Form f in frmdal.Forms
+                                 where b.Contains(f.NameOfUser)
+                                 select f).ToList<Form>();
+            VMForms fff = new VMForms { forms = formss };
+            return View(fff);
         }
-    }
 
-    public ActionResult ReciverMessages()
-    {
-        if (!Authorize())
-            return RedirectToAction("RedirectByUser", "Home");
-        VMMessages msgs = new VMMessages();
-        MessageDal msDal = new MessageDal();
-        User curr = (User)Session["CurrentUser"];
-        msgs.Messages = (from msg in msDal.messages
-                         where msg.Receiver == curr.UserName
-                         select msg).ToList<Message>();
-        return View(msgs);
-    }
-    public ActionResult NewMessage()
-    {
-        if (!Authorize())
-            return RedirectToAction("RedirectByUser", "Home");
-        return View(new Message());
-    }
-    public ActionResult SendMessage(Message msg)
-    {
-        if (!Authorize())
-            return RedirectToAction("RedirectByUser", "Home");
-        User CurrentUser = (User)Session["CurrentUser"];
-        UserDal usDal = new UserDal();
-        if (usDal.Users.FirstOrDefault<User>(x => x.UserName == msg.Receiver) == null)
+        [HttpGet]
+        public ActionResult ShowForm(string name)
         {
-            TempData["notUser"] = "לא קיים משתמש!";
-            return RedirectToAction("NewMessage");
+            if (Session["CurrentUser"] == null)
+                return RedirectToAction("RedirectByUser", "Home");
+            FormDal frmdal = new FormDal();
+            Form form = frmdal.Forms.FirstOrDefault<Form>(x => x.NameOfProject == name);
+            return View(form);
         }
 
-        MessageDal msDal = new MessageDal();
-        msg.Sender = CurrentUser.UserName;
-        msg.DateAndTime = DateTime.Now;
-        msDal.messages.Add(msg);
-        msDal.SaveChanges();
-        TempData["OK"] = "הודעה נשלחה למשתמש";
-        return RedirectToAction("NewMessage");
+
+
+
+        public ActionResult MessagesPage()
+        {
+            {
+                if (!Authorize())
+                    return RedirectToAction("RedirectByUser", "Home");
+                return View();
+            }
+        }
+
+        public ActionResult ReciverMessages()
+        {
+            if (!Authorize())
+                return RedirectToAction("RedirectByUser", "Home");
+            VMMessages msgs = new VMMessages();
+            MessageDal msDal = new MessageDal();
+            User curr = (User)Session["CurrentUser"];
+            msgs.Messages = (from msg in msDal.messages
+                             where msg.Receiver == curr.UserName
+                             select msg).ToList<Message>();
+            return View(msgs);
+        }
+        public ActionResult NewMessage()
+        {
+            if (!Authorize())
+                return RedirectToAction("RedirectByUser", "Home");
+            return View(new Message());
+        }
+        public ActionResult SendMessage(Message msg)
+        {
+            if (!Authorize())
+                return RedirectToAction("RedirectByUser", "Home");
+            User CurrentUser = (User)Session["CurrentUser"];
+            UserDal usDal = new UserDal();
+            if (usDal.Users.FirstOrDefault<User>(x => x.UserName == msg.Receiver) == null)
+            {
+                TempData["notUser"] = "לא קיים משתמש!";
+                return RedirectToAction("NewMessage");
+            }
+
+            MessageDal msDal = new MessageDal();
+            msg.Sender = CurrentUser.UserName;
+            msg.DateAndTime = DateTime.Now;
+            msDal.messages.Add(msg);
+            msDal.SaveChanges();
+            TempData["OK"] = "הודעה נשלחה למשתמש";
+            return RedirectToAction("NewMessage");
+
+        }
 
     }
-
-}
 }
